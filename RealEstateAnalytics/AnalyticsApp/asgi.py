98f182f60fb2a45d6337ba7923b80f501fb1d4eb
phoenixp123 <ppagan@clarku.edu>
2021-05-26 16:20:53 -0400

    attempting to fix naming problem

--- RealEstateAnalytics/AnalyticsApp/asgi.py
+++ RealEstateAnalytics/AnalyticsApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RealEstateAnalytics project.
+ASGI config for AnalyticsApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RealEstateAnalytics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnalyticsApp.settings')
 
 application = get_asgi_application()
--- RealEstateAnalytics/AnalyticsApp/wsgi.py
+++ RealEstateAnalytics/AnalyticsApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RealEstateAnalytics project.
+WSGI config for AnalyticsApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RealEstateAnalytics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnalyticsApp.settings')
 
 application = get_wsgi_application()
