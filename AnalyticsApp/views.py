befc689ec55b4303d24909c2a1be73ea209e5b56
Nathan Tran <natran411@gmail.com>
2021-05-31 18:48:03 -0400

    attempt plotly html

---
+++ AnalyticsApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+
+
+def home(request):
+    x_data = [0, 1, 2, 3]
+    y_data = [x ** 2 for x in x_data]
+    plot_div = plot([Scatter(x=x_data, y=y_data,
+                             mode='lines', name='test',
+                             opacity=0.8, marker_color='green')],
+                    output_type='div')
+
+    return render(request, "home.html", context={'plot_div': plot_div})
