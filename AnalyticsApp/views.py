32a9b71c07ac2a0985536fd0dc721ae38e2859ec
phoenixp123 <ppagan@clarku.edu>
2021-05-31 21:51:48 -0400

    changed views location

--- AnalyticsApp/views.py
+++ AnalyticsApp/views.py
@@ -8,7 +8,7 @@ def home(request):
     y_data = [x ** 2 for x in x_data]
     plot_div = plot([Scatter(x=x_data, y=y_data,
                              mode='lines', name='test',
-                             opacity=0.8, marker_color='green')],
+                             opacity=0.8)],
                     output_type='div')
 
     return render(request, "home.html", context={'plot_div': plot_div})
